e853224747d044a7a82623998e0be72f6ead317f
Heitor Repolho <heitor.repolho@verizonconnect.com>
2020-04-10 21:44:51 +0100

    Fetch customer data when page is loaded

--- src/main/webapp/javascript/options.js
+++ src/main/webapp/javascript/options.js
@@ -1,6 +1,15 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+$(function () {
+    if (isCustomerLoggedIn() == false) { return; }
+
+    let customerIdentifier = getCustomerIdentifier();
+
+    $.getJSON('api/onlinebanking/customer/' + customerIdentifier, function (customer) {
+        $('#customerData').html(JSON.stringify(customer));
+    });
+});
