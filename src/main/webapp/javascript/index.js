412ef735484531ec3248ff157c839bbd107beb24
Heitor Repolho <heitor.repolho@verizonconnect.com>
2020-04-10 21:42:24 +0100

    Do not call /createCustomers
    Customers are already created when server is launched.

--- src/main/webapp/javascript/index.js
+++ src/main/webapp/javascript/index.js
@@ -1,15 +1,6 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function loadData(){
-   
-     var stringUrl = "http://localhost:49000/api/onlinebanking/createCustomers";
-   $.ajax({ 
-       url: stringUrl
-   });
-    
-}
-
-
+function loadData() { }
