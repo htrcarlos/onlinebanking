7e380cf6856210da3388d193bf43d2c339c7adfe
Heitor Repolho <heitor.repolho@verizonconnect.com>
2020-04-10 21:38:49 +0100

    Fix CustomerResource
    - Add GET /customer/{identifier} endpoint
    - Fix login endpoint

--- src/main/java/com/mycompany/onlinebanking/resources/CustomerResource.java
+++ src/main/java/com/mycompany/onlinebanking/resources/CustomerResource.java
@@ -9,6 +9,8 @@ import com.google.gson.Gson;
 import com.mycompany.onlinebanking.model.Account;
 import com.mycompany.onlinebanking.model.Customer;
 import com.mycompany.onlinebanking.model.Transaction;
+import com.mycompany.onlinebanking.request.LoginRequestData;
+import com.mycompany.onlinebanking.request.RequestError;
 import com.mycompany.onlinebanking.services.CustomerService;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -34,17 +36,19 @@ import javax.ws.rs.core.Response;
 public class CustomerResource extends HttpServlet {
 
     CustomerService cs = new CustomerService();
+    Gson gson = new Gson();
 
-    //curl -vi -X GET -G "http://localhost:49000/api/onlinebanking/createCustomers"
     @GET
-    @Path("/createCustomers")
+    @Path("/customer/{identifier}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createCustomers() {
+    public Response getCustomer(@PathParam("identifier") int identifier) {
+        Customer c = cs.getCustomer(identifier);
 
-        Gson gson = new Gson();
-        List<Customer> customers = cs.createCustomers();
-        System.out.println("Entrou create costumers " + customers.size());
-        return Response.status(Response.Status.CREATED).entity(gson.toJson(customers)).build();
+        if (c != null) {
+            return Response.status(Response.Status.OK).entity(gson.toJson(c)).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).entity(gson.toJson(RequestError.customerNotFound)).build();
+        }
     }
 
     //curl -v -X POST "http://localhost:49000/api/onlinebanking/createCustomer" -d {"name":"'Freddie Mercury'","address":"'35 Liffey Street South'","email":"'freddie@gmail.com'","credentials":123}
@@ -52,7 +56,6 @@ public class CustomerResource extends HttpServlet {
     @Path("/createCustomer")
     @Produces(MediaType.APPLICATION_JSON)
     public Response createCustomer(String body) throws UnsupportedEncodingException, IOException {
-        Gson gson = new Gson();
         Customer c = gson.fromJson(body, Customer.class);
         Customer customer = cs.createCustomer(c);
         System.out.println(cs.getList().size());
@@ -62,7 +65,6 @@ public class CustomerResource extends HttpServlet {
     @POST
     @Path("{param}/createAccount")
     public Response createAccount(@PathParam("param") int sortCode, @QueryParam("email") String email) {
-        Gson gson = new Gson();
         Account account = cs.createAccount(email, sortCode);
 
         return Response.status(Response.Status.CREATED).entity(gson.toJson(account)).build();
@@ -72,7 +74,6 @@ public class CustomerResource extends HttpServlet {
     @Path("{sortCode}/{accountNumber}/newLodgement")
     @Produces(MediaType.APPLICATION_JSON)
     public Response newLodgement(@PathParam("sortCode") int sortCode, @PathParam("accountNumber") int accountNumber, @QueryParam("value") double value) {
-        Gson gson = new Gson();
         Transaction transaction = cs.newLodgement(sortCode, accountNumber, value);
 
         return Response.status(Response.Status.CREATED).entity(gson.toJson(transaction)).build();
@@ -83,7 +84,6 @@ public class CustomerResource extends HttpServlet {
     @Path("{sortCode}/{accountNumber}/newWithdrawal")
     @Produces(MediaType.APPLICATION_JSON)
     public Response newWithdrawal(@PathParam("sortCode") int sortCode, @PathParam("accountNumber") int accountNumber, @QueryParam("value") double value) {
-        Gson gson = new Gson();
         Transaction transaction = cs.newWithdrawal(sortCode, accountNumber, value);
 
         return Response.status(Response.Status.CREATED).entity(gson.toJson(transaction)).build();
@@ -94,7 +94,6 @@ public class CustomerResource extends HttpServlet {
     @Path("{sortCode}/{accountNumber}/getBalance")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBalance(@PathParam("sortCode") int sortCode, @PathParam("accountNumber") int accountNumber) {
-        Gson gson = new Gson();
         Double currentBalance = cs.getBalance(sortCode, accountNumber);
 
         return Response.status(Response.Status.CREATED).entity(gson.toJson(currentBalance)).build();
@@ -105,7 +104,6 @@ public class CustomerResource extends HttpServlet {
     @Path("{sortCode}/{accountNumber}/newTransfer")
     @Produces(MediaType.APPLICATION_JSON)
     public Response newTransfer(@PathParam("sortCode") int sortCode, @PathParam("accountNumber") int accountNumber, @QueryParam("destinSortCode") int destinSortCode, @QueryParam("destinAccountNumber") int destinAccountNumber, @QueryParam("value") double value) {
-        Gson gson = new Gson();
         List<Transaction> transaction = cs.newTransfer(sortCode, accountNumber, destinSortCode, destinAccountNumber, value);
 
         return Response.status(Response.Status.CREATED).entity(gson.toJson(transaction)).build();
@@ -115,7 +113,6 @@ public class CustomerResource extends HttpServlet {
     @Path("{email}/transactionHistory")
     @Produces(MediaType.APPLICATION_JSON)
     public Response transactionHistory(@PathParam("email") String email, @QueryParam("sortCode") int sortCode, @QueryParam("accountNumber") int accountNumber) {
-        Gson gson = new Gson();
         List<Transaction> transactions = cs.transactionHistory(sortCode, accountNumber);
 
         return Response.status(Response.Status.CREATED).entity(gson.toJson(transactions)).build();
@@ -124,12 +121,14 @@ public class CustomerResource extends HttpServlet {
     @POST
     @Path("/customerLogin")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response customerLogin(@QueryParam("email") String email, @QueryParam("credentials") int credentials) {
-       
-        Gson gson = new Gson();
-        Customer customer = cs.customerLogin(email, credentials);
-        
-        return Response.status(Response.Status.CREATED).entity(gson.toJson(gson.toJson(customer))).build();
-    }
+    public Response customerLogin(String body) {
+        LoginRequestData requestData = gson.fromJson(body, LoginRequestData.class);
+        Customer customer = cs.customerLogin(requestData.email, requestData.credentials);
 
+        if (customer != null) {
+            return Response.status(Response.Status.OK).entity(gson.toJson(customer)).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).entity(gson.toJson(RequestError.customerNotFound)).build();
+        }
+    }
 }
